f18f0959dee2d496777f0d340880a7227f3b864f
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-07-21 18:27:47 +0100

    created login
    added path in urls.py
    correctet login_ urls in settings

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name='home'), 
     path('new/', views.new_post, name='new_post'),
     path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
 ]
 
