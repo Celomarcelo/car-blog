8b010546de81721deaa11f6a5fc01e22a9e8ca98
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-07-22 11:24:09 +0100

    created logout section
    added post form in base html
    moved log out html
    changed logout urls settings

--- CarBlog/settings.py
+++ CarBlog/settings.py
@@ -137,7 +137,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'logout'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
