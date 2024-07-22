f91ca2148d8c702af72d7842bafe54a1740cafae
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-07-22 13:32:43 +0100

    added path for post delete
    added path in urls.py
    added <a> in post details

--- blog/urls.py
+++ blog/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('user/<str:username>/posts/', views.user_posts, name='user_posts'),
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/delete/', views.post_delete, name='post_delete'),
 ]
 
