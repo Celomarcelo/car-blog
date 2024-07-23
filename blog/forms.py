2929355db075cc8fb1d426d042cfcbb0967e0f44
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-07-23 18:28:48 +0100

    update forms .py
    added email for registration

--- blog/forms.py
+++ blog/forms.py
@@ -12,9 +12,17 @@ class PostForm(forms.ModelForm):
             'content': forms.Textarea(attrs={'rows': 5}),
         }
 
+
 class UserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    
+
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
