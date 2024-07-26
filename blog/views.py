10db9bb7d95c6a3ac7c8fa52971ca455461b1431
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-07-26 18:35:13 +0100

    update views py
    delete login required for post details

--- blog/views.py
+++ blog/views.py
@@ -39,7 +39,7 @@ def user_posts(request, username):
     posts = Post.objects.filter(author=user)
     return render(request, 'user_posts.html', {'user': user, 'posts': posts})
 
-@login_required
+
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     comments = post.comments.filter(approved=True)
@@ -61,7 +61,7 @@ def post_detail(request, post_id):
         'comment_form': comment_form,
     })
 
-@login_required
+
 def post_edit(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     if request.method == 'POST':
@@ -73,7 +73,7 @@ def post_edit(request, post_id):
         form = PostForm(instance=post)
     return render(request, 'post_edit.html', {'form': form, 'post': post})
 
-@login_required
+
 def post_delete(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
     if request.method == 'POST':
