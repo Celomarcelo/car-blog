77ac84154f4ca101061858ea545ee95624e5c51d
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-07-27 17:23:03 +0100

    created tests
    post
    post creation
    comment
    comment creation

--- blog/tests.py
+++ blog/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
-from .models import Category
+from .models import Category, Post, Comment
+from django.contrib.auth.models import User
 
 
 class CategoryModelTest(TestCase):
@@ -8,4 +9,46 @@ class CategoryModelTest(TestCase):
 
     def test_category_str(self):
         self.assertEqual(str(self.category), "Technology")
+        
+class PostModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.category = Category.objects.create(name='Django')
+        self.post = Post.objects.create(
+            title='Django Testing',
+            content='Content of the post',
+            author=self.user,
+            category=self.category
+        )
+
+    def test_post_creation(self):
+        self.assertEqual(self.post.title, 'Django Testing')
+        self.assertEqual(self.post.content, 'Content of the post')
+        self.assertEqual(self.post.author.username, 'testuser')
+        self.assertEqual(self.post.category.name, 'Django')
+        self.assertFalse(self.post.approved)
+        self.assertEqual(str(self.post), 'Django Testing')
+
+class CommentModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpass')
+        self.category = Category.objects.create(name='Django')
+        self.post = Post.objects.create(
+            title='Django Testing',
+            content='Content of the post',
+            author=self.user,
+            category=self.category
+        )
+        self.comment = Comment.objects.create(
+            post=self.post,
+            author=self.user,
+            content='This is a comment'
+        )
+
+    def test_comment_creation(self):
+        self.assertEqual(self.comment.post.title, 'Django Testing')
+        self.assertEqual(self.comment.author.username, 'testuser')
+        self.assertEqual(self.comment.content, 'This is a comment')
+        self.assertFalse(self.comment.approved)
+        self.assertEqual(str(self.comment), f'Comment by {self.comment.author} on {self.comment.post}')
 
