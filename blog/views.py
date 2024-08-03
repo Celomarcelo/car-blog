29fd7c4221fabe5219b3da218ebbe92663430da0
Celomarcelo <150127169+Celomarcelo@users.noreply.github.com>
2024-08-03 20:19:19 +0100

    delete tratment of img in views
    added loading in home, post details html

--- blog/views.py
+++ blog/views.py
@@ -29,12 +29,6 @@ def new_post(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            if 'image' in request.FILES:
-                image_file = request.FILES['image']
-                result = upload(image_file, transformation=[
-                    {'width': 800, 'height': 600, 'crop': 'limit', 'quality': 'auto', 'fetch_format': 'auto'}
-                ])
-                post.image = result
             post.save()
             return redirect('home')
     else:
